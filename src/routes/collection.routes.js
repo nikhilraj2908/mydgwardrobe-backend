60856b0161629698b95a6eb3254af4da2195af2f
nikhil ahirwar <nikhilraj2908@gmail.com>
2025-12-24 18:39:12 +0530

    corrected code

--- src/routes/collection.routes.js
+++ src/routes/collection.routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const {
   getUserCollections,
-  getUserWardrobesWithPublicStats
+//   getUserWardrobesWithPublicStats,
+  getUserWardrobesWithStats
 } = require("../controllers/collection.controller");
 
 // GET /api/collections/:userId
