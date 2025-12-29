3495ad9f3892619dd8c16235ad3c4597cc53bb49
nikhil ahirwar <nikhilraj2908@gmail.com>
2025-12-29 17:43:53 +0530

    added limit in server.json

--- server.js
+++ server.js
@@ -32,8 +32,12 @@ app.use((req, res, next) => {
 });
 
 /* ================= BODY PARSERS ================= */
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+
 
 /* ================= DB ================= */
 connectDB();
